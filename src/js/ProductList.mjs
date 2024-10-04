56677e70909f63a58fdae737eba9e282af8c2a90
Bernie Zhong <chu23008@byui.edu>
2024-10-04 19:44:49 +0800

    stretch done(?)

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,32 +1,9 @@
-export default class ProductListing {
-    constructor(category, dataSouce, listElement) {
-        // We passed in this information to make our class as reusable as possible.
-        // Being able to define these things when we use the class will make it very flexible
-        this.category = category;   // products class 
-        this.dataSouce = dataSouce; 
-        this.listElement = listElement; // HTML element
-    }
-
-    async init() {
-        // our dataSource will return a Promise...so we can use await to resolve it.
-        const list = await this.dataSouce.getData();
-        this.renderProductList(list);
-    }
-
-    renderProductList(products) {
-        this.listElement.innerHTML = '';
-    
-        products.forEach((product) => {
-            const cardHTML = productCardTemplate(product);
-            this.listElement.insertAdjacentHTML('beforeend', cardHTML);
-        });
-    }
-}
+import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `
         <li class="product-card">
-        <a href="product_pages/index.html?product=${product.id}">
+        <a href="product_pages/index.html?product=${product.Id}">
             <img src="${product.imageUrl}" alt="Image of ${product.name}">
             <h3 class="card__brand">${product.brand}</h3>
             <h2 class="card__name">${product.name}</h2>
@@ -34,4 +11,29 @@ function productCardTemplate(product) {
         </a>
         </li>
     `;
+}
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+        // We passed in this information to make our class as reusable as possible.
+        // Being able to define these things when we use the class will make it very flexible
+        this.category = category;   // products class 
+        this.listElement = listElement; // HTML element
+        this.dataSource = dataSource; 
+    }
+
+    filterProducts(products, count = 4) {
+        return products.slice(0, count);
+    }
+
+    async init() {
+        // our dataSource will return a Promise...so we can use await to resolve it.
+        const list = await this.dataSource.getData();
+        const filteredList = this.filterProducts(list, 4);
+        this.renderList(filteredList);
+    }
+
+    renderList(products) {
+        renderListWithTemplate(productCardTemplate, this.listElement, products, "afterbegin", ture);
+    }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,5 @@ const category = 'tents'
 const productData = new ProductData(category);
 const listElement = document.querySelector(`#product-list`);
 
-async function displayProducts() {
-    try {
-        const products = await productData.getData();
-        console.log(products);
-    } catch (error) {
-        console.error('error when getting products', error);
-    }
-}
-
-displayProducts();
-
 const productListing = new ProductListing(category, productData, listElement);
 productListing.init();
